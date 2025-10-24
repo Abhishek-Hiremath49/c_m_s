efe9075a00f85fa09fa4434c6c023e3c00cc423d
Abhi <abhishekhiremath4949@gmail.com>
2025-10-24 10:37:56 +0000

    deleted user doctype

--- cms/course_management_system/doctype/user/user.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Abhi and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class User(Document):
-	pass
