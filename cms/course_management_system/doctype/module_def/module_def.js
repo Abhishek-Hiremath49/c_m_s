02914c08dea5838d95ae2d5c19c4bdd6da03f1b4
Abhi <abhishekhiremath4949@gmail.com>
2025-10-24 11:05:12 +0000

    deleted module def and role profile

--- cms/course_management_system/doctype/module_def/module_def.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Abhi and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Module Def", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- cms/course_management_system/doctype/role_profile/role_profile.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Abhi and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class RoleProfile(Document):
-	pass
